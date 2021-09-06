fc6556efc1bdb9cad1b08de398aec746549f54c6
rushidange45 <87379113+rushidange45@users.noreply.github.com>
2021-09-06 22:45:52 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from './Component/Header';
+import Content from './Component/Content';
+import './App.css';
+
+const App = () => {
+    return (
+        <>
+            <Header />
+            <Content />
+        </>
+    );
+};
+
+export default App
